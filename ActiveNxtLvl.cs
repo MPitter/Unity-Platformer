8002e7cfc03cfa47de8264f3c6a050cef79b704e
MPitter <88744644+MPitter@users.noreply.github.com>
2021-08-10 16:27:24 -0400

    Add files via upload

---
+++ ActiveNxtLvl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveNxtLvl : MonoBehaviour {
+    ///Space Paranoids related
+    NextLevel nl;
+    
+	// Use this for initialization
+	void Start () {
+        nl = FindObjectOfType<NextLevel>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            nl.enabled = true;
+        }
+    }
+        }
